e0d394a7b83b9cbbbf86199a1a78b33e99dcc7b8
toma satou <84651853+sato-toma@users.noreply.github.com>
2025-01-14 01:10:59 +0900

    add Speed Down

---
+++ Editor/Script/SimpleItem/CraftItem/SpeedDownWepon/speed_down.js
@@ -0,0 +1,51 @@
+
+
+$.onStart(() => {
+    $.state.grabbingPlayer = null;
+    $.state.grabbingLeft = true;
+});
+
+$.onGrab((isGrab, isLeft, player) => {
+    if (isGrab) $.state.grabbingPlayer = player;
+    else $.state.grabbingPlayer = null;
+    $.state.grabbingLeft = isLeft;
+});
+
+const onCollide = () => {
+    let _overlapPlayers = [];
+    const speedL = 0.4;
+    const speedR = 0.7;
+    return ($) => {
+        let grabbingPlayer = $.state.grabbingPlayer;
+        if (grabbingPlayer == null || !grabbingPlayer.exists()) {
+            _overlapPlayers = [];
+            return;
+         }
+        const grabbingLeft = $.state.grabbingLeft;
+        let previousOverlapPlayers = _overlapPlayers;
+        let currentOverlapPlayers = [];
+
+        let collisions = $.getOverlaps();
+        for (let collision of collisions) {
+            let playerHandle = collision.handle;
+            if (playerHandle == null || playerHandle?.type !== "player") return;
+            if (playerHandle.id == grabbingPlayer.id) return;
+
+            currentOverlapPlayers.push(playerHandle.id);
+            if (previousOverlapPlayers.includes(playerHandle.id)) return;
+            if (grabbingLeft) {
+                playerHandle.setMoveSpeedRate(speedL);
+            }
+            else {
+                playerHandle.setMoveSpeedRate(speedR);
+             }
+        }
+
+        _overlapPlayers = currentOverlapPlayers;
+    }
+};
+const handleCollisions = onCollide();
+
+$.onUpdate(deltaTime => {
+    handleCollisions($);
+});
