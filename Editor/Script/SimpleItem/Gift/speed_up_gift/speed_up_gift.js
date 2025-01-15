e3213ac67fea22892ed87cf25e435c59c795600e
toma satou <84651853+sato-toma@users.noreply.github.com>
2025-01-16 01:07:30 +0900

    gift spped up

---
+++ Editor/Script/SimpleItem/Gift/speed_up_gift/speed_up_gift.js
@@ -0,0 +1,13 @@
+$.onGiftSent((gifts) => {
+    $.log("onGiftSent called");
+    for (let i = 0; i < gifts.length; i++) {
+        let player = gifts[i].sender;
+        let price = gifts[i].price;
+        $.log("push player:" + player);
+        if (player != null) {
+            $.log("push player.Userid:" + player.userId);
+            player.setMoveSpeedRate(price);
+        }
+    }
+});
+
