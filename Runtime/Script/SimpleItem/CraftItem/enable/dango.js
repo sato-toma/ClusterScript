258c7eb5061da772fc29256c0445e73e6b760a7d
toma satou <84651853+sato-toma@users.noreply.github.com>
2025-03-01 01:01:18 +0900

    change dango enabled logic

--- Runtime/Script/SimpleItem/CraftItem/enable/dango.js
+++ Runtime/Script/SimpleItem/CraftItem/enable/dango.js
@@ -5,16 +5,25 @@ const subNodes = [
 ]
 const subNodesSize = subNodes.length;
 let nodeIndex = 0;
+let toggleMode = true;
 $.onUse((isDown, player) => {
     // $.log(`colorIndex=${colorIndex}`);
     if (!isDown) { return; }
     if ($.getGrabbingPlayer() === null) { return; }
 
-    const node = subNodes[nodeIndex];
-    let enabled = node.getEnabled();
-    node.setEnabled(!enabled);
+    if (toggleMode) {
+        const node = subNodes[nodeIndex];
+        let enabled = node.getEnabled();
+        node.setEnabled(!enabled);
 
-    nodeIndex = (nodeIndex + 1) % subNodesSize; // 0 -> 1 -> 2 ...
+        nodeIndex = (nodeIndex + 1) % subNodesSize; // 0 -> 1 -> 2 ...
+        if (nodeIndex === 0) {
+            toggleMode = false;
+        }
+    } else {
+        subNodes.forEach(n => n.setEnabled(true));
+        toggleMode = true;
+    }
 });
 
 
